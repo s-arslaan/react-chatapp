774b106ad19572e18d8377415fd32f9f5587b783
s-arslaan <shaikharslaan1234@gmail.com>
2022-07-27 00:23:57 +0530

    join

--- src/Components/Join/Join.js
+++ src/Components/Join/Join.js
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import "./Join.css";
+import logo from "../../images/logo.png";
+import { Link } from "react-router-dom";
+
+let User;
 
 function Join() {
+  const sendUser = () => {
+    User = document.getElementById("joinInput").value;
+  };
+
   return (
-    <h1>Join</h1>
-  )
+    <div className="JoinPage">
+      <div className="JoinContainer">
+        <img src={logo} alt="logo" />
+        <h1>Chat App</h1>
+        <input
+          placeholder="Enter your name"
+          type="text"
+          name=""
+          id="joinInput"
+        />
+        <Link to="/chat">
+          <button onClick={sendUser} className="joinbtn">
+            Login In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 }
 
-export default Join
+export default Join;
+export { User };
